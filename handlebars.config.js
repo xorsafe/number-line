4711085eb9f1857442e9b66474fe64fd197ccff6
Nikhil Verma <nikhilnxvverma1@gmail.com>
2022-07-27 11:35:20 +0530

    Removed unneeded files from last merge

--- handlebars.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	data: 'src/handlebars/data',
-	decorators: 'src/handlebars/decorators',
-	helpers: 'src/handlebars/helpers',
-	layouts: 'src/handlebars/layouts',
-	partials: 'src/handlebars/partials',
-};
