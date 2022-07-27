6d72a16fd9b92ebb096dd18df8de326ccf3525cb
Nikhil Verma <nikhilnxvverma1@gmail.com>
2022-07-27 18:41:09 +0530

    Integrated number line but  using personal file

--- number-line/pages/posts/first-post.tsx
+++ number-line/pages/posts/first-post.tsx
@@ -1,25 +1,19 @@
 import Head from "next/head";
 import Link from "next/link";
-import Script from "next/script";
+import Layout from "../../components/layout";
+
 export default function FirstPost() {
 	return (
-		<>
-		 <Head>
-			<title>First Post</title>
-			<Script
-				src="https://connect.facebook.net/en_US/sdk.js"
-				strategy="lazyOnload"
-				onLoad={() =>
-				console.log(`script loaded correctly, window.FB has been populated`)
-				}
-			/>
-		</Head>
+		<Layout>
+			<Head>
+				<title>First Post</title>
+			</Head>
 			<h1>First Post</h1>
 			<h2>
 				<Link href="/">
-					Back to home
+					<a>Back to home</a>
 				</Link>
 			</h2>
-		</>
+		</Layout>
 	);
 }
