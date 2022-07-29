6286bbaccf65b4c7dcc81152aa48def8335416d3
Nikhil Verma <nikhilnxvverma1@gmail.com>
2022-07-29 18:02:43 +0530

    Number line react component with label on ticks

--- number-line/pages/index.tsx
+++ number-line/pages/index.tsx
@@ -1,13 +1,15 @@
+import { AppPropsType } from 'next/dist/shared/lib/utils';
 import Head from 'next/head';
+import { ScriptProps } from 'next/script';
 import React, { createRef, ReactNode } from 'react';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import { NumberLine, ITickMarkLabelStrategy, NumberLineViewModel} from './my-number-line';
+import { NumberLine, ITickMarkLabelStrategy, NumberLineViewModel, TickMarkViewModel} from './my-number-line';
 
 const numberScaleTickMarkStrategy:ITickMarkLabelStrategy={
 	labelFor(value,index,position,numberLine){
 		if(index==0 || index ==5){
-			return value+"";
+			return Math.round(value)+"";
 		}
 		return "";
 	}
@@ -22,58 +24,76 @@ const numberScale:NumberLine = new NumberLine({
 
 })
 
-const NumberScaleTickMarks=(props:{model:NumberLineViewModel}):JSX.Element=>{
-	let keyCounter=0;
-	return (
-		<div className='flex' style={{columnGap:props.model.gap-1}}>
-			{
-				props.model.tickMarks.map((v,i,tvm)=>{
-					if(v.patternIndex==0){
-						return (<div key={keyCounter++ +""} className={'h-3 bg-black'} style={{width:"1px"}}></div>)
-					}else if(v.patternIndex==5){
-						return (<div key={keyCounter++ +""} className={'h-2 bg-black'} style={{width:"1px"}}></div>)
-					}else{
-						return (<div key={keyCounter++ +""} className={'h-1 bg-black'} style={{width:"1px"}}></div>)
-					}
-				})
-			}
-		</div>
-	)
+interface NumberLineState{
+	model:NumberLineViewModel;
 }
+
+class NumberScaleTickMarks extends React.Component<NumberLineState,NumberLineState>{
+	constructor(props:NumberLineState){
+		super(props);
+		this.state = {model:props.model}
+	}
+
+	private tickMarkLeftClassName(v:TickMarkViewModel):string{
+		if(v.label!=null){
+			switch(v.label.length){
+				case 1: return "-left-1"
+				case 2: return "-left-2"
+				case 3: return "-left-3"
+				case 4: return "-left-3"
+				case 5: return "-left-4"
+				default: return "-left-5"
+			}
+		}
+		return "";
+	}
+
+	render(): React.ReactNode {
+		let keyCounter=0;
+		return (
+			<div className='flex items-end' style={{columnGap:this.state.model.gap-1}}>
+				{
+					this.state.model.tickMarks.map((v,i,tvm)=>{
+						if(v.patternIndex==0){
+							const leftClassName = this.tickMarkLeftClassName(v);
+							return (
+							<div key={keyCounter++ +""} className={'relative h-3 bg-black'} style={{width:"1px"}}>
+								<div className={"text-xs absolute -top-6 "+leftClassName}>{v.label}</div>
+							</div>)
+						}else if(v.patternIndex==5){
+							return (<div key={keyCounter++ +""} className={'h-2 bg-black'} style={{width:"1px"}}></div>)
+						}else{
+							return (<div key={keyCounter++ +""} className={'h-1 bg-black'} style={{width:"1px"}}></div>)
+						}
+					})
+				}
+			</div>
+		)
+	}
+}
+
 
 class Home extends React.Component {
 
 	resizeObserver!:ResizeObserver;
 	resizeElement:React.RefObject<HTMLDivElement> = createRef();
+	numberLineContainer:React.RefObject<NumberScaleTickMarks> = createRef();
 
 	numberLineViewModel!:NumberLineViewModel;
 
-	constructor(){
-		super({});
+	constructor(props:any){
+		super(props);
 		// initial view model will be updated later
 		this.numberLineViewModel = numberScale.buildViewModel(0)
 	}
 
 	componentDidMount(){
 
-		
 		this.resizeObserver = new ResizeObserver((entries)=>{
-			let width:number =0;
-			let height:number =0;
 			
 			for (let entry of entries) {
-				// if(entry.contentBoxSize) {
-				//   // Firefox implements `contentBoxSize` as a single content rect, rather than an array
-				//   const contentBoxSize = Array.isArray(entry.contentBoxSize) ? entry.contentBoxSize[0] : entry.contentBoxSize;
-					
-				//   width= contentBoxSize.width;
-				//   height = contentBoxSize.height;
-				// } else {
-				// 	width= entry.contentRect.width;
-				// 	height = entry.contentRect.height;
-				// }
 				console.log("Rebuilding number line view model for width "+entry.contentRect.width);
-				this.numberLineViewModel = numberScale.buildViewModel(entry.contentRect.width)
+				this.numberLineContainer.current?.setState({model:numberScale.buildViewModel(entry.contentRect.width)})
 			}
 		});
 
@@ -92,19 +112,19 @@ class Home extends React.Component {
 		return (
 			<>
 				<Layout home>
-				<Head>
+					<Head>
 					<title>{siteTitle}</title>
-				</Head>
-				<section className={'text-center '+utilStyles.headingMd}>
-					<p>Virtually infinite,pannable,zoomable and completely customizable</p>
-					<p>
-					Drag the number line below with your mouse/trackpad. Ctrl + Mousewheel for zooming
-					</p>
-					<a href="https://github.com/nikhilnxvverma1/number-line">Github</a>
-				</section>
+					</Head>
+					<section className={'text-center '+utilStyles.headingMd}>
+						<p>Virtually infinite,pannable,zoomable and completely customizable</p>
+						<p>
+						Drag the number line below with your mouse/trackpad. Ctrl + Mousewheel for zooming
+						</p>
+						<a href="https://github.com/nikhilnxvverma1/number-line">Github</a>
+					</section>
 				</Layout>
-				<div ref={this.resizeElement} className="w-full h-12 bg-red-300">
-					<NumberScaleTickMarks model={this.numberLineViewModel}></NumberScaleTickMarks>
+				<div ref={this.resizeElement} className="w-full h-12 flex flex-col justify-end bg-slate-50">
+					<NumberScaleTickMarks ref={this.numberLineContainer} model={this.numberLineViewModel}></NumberScaleTickMarks>
 				</div>
 			</>
 	  )
