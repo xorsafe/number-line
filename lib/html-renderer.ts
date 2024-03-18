e637c2f336d53baf40069c5a2943c7a92f459ca1
Nikhil Verma <nikhilnxvverma1@gmail.com>
2024-03-18 18:52:03 +0530

    Created foundational files with sufficient grounding html for future work

---
+++ lib/html-renderer.ts
@@ -0,0 +1,7 @@
+import { NumberLineViewModel } from "number-line";
+
+export function render(viewModel:NumberLineViewModel,container:HTMLElement) {
+	const div = document.createElement("div");
+	div.innerHTML = "html";
+	return div;
+}
